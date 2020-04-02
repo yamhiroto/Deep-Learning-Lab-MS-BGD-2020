36b87691abe1cff1eaf92355c9ee431950372a39
Hiroto <yam.hiroto@gmail.com>
2020-04-02 09:32:54 +0200

    Deep Learning Lab

---
+++ TP09 - Pytorch GAN - 18.03.2020/correction.py
@@ -0,0 +1,136 @@
+
+
+
+class Generator(nn.Module):
+    def __init__(self, g_input_dim, g_output_dim):
+        super(Generator, self).__init__()       
+        self.fc1 = nn.Linear(g_input_dim, 256)
+        self.fc2 = nn.Linear(self.fc1.out_features, self.fc1.out_features*2)
+        self.fc3 = nn.Linear(self.fc2.out_features, self.fc2.out_features*2)
+        self.fc4 = nn.Linear(self.fc3.out_features, g_output_dim)
+    
+    # forward method
+    def forward(self, x): 
+        x = F.leaky_relu(self.fc1(x), 0.2)
+        x = F.leaky_relu(self.fc2(x), 0.2)
+        x = F.leaky_relu(self.fc3(x), 0.2)
+        return torch.tanh(self.fc4(x))
+    
+class Discriminator(nn.Module):
+    def __init__(self, d_input_dim):
+        super(Discriminator, self).__init__()
+        self.fc1 = nn.Linear(d_input_dim, 1024)
+        self.fc2 = nn.Linear(self.fc1.out_features, self.fc1.out_features//2)
+        self.fc3 = nn.Linear(self.fc2.out_features, self.fc2.out_features//2)
+        self.fc4 = nn.Linear(self.fc3.out_features, 1)
+    
+    # forward method
+    def forward(self, x):
+        x = F.leaky_relu(self.fc1(x), 0.2)
+        x = F.dropout(x, 0.3)
+        x = F.leaky_relu(self.fc2(x), 0.2)
+        x = F.dropout(x, 0.3)
+        x = F.leaky_relu(self.fc3(x), 0.2)
+        x = F.dropout(x, 0.3)
+        return torch.sigmoid(self.fc4(x))
+
+    
+# build network
+z_dim = 100
+mnist_dim = train_dataset.train_data.size(1) * train_dataset.train_data.size(2)
+
+G = Generator(g_input_dim = z_dim, g_output_dim = mnist_dim).to(device)
+D = Discriminator(mnist_dim).to(device)
+
+# loss
+criterion = nn.BCELoss() 
+
+# optimizer
+lr = 0.0002 
+G_optimizer = optim.Adam(G.parameters(), lr = lr)
+D_optimizer = optim.Adam(D.parameters(), lr = lr)
+
+def D_train(x):
+    #=======================Train the discriminator=======================#
+    D.zero_grad()
+
+    # train discriminator on real
+    x_real, y_real = x.view(-1, mnist_dim), torch.ones(bs, 1)
+    x_real, y_real = Variable(x_real.to(device)), Variable(y_real.to(device))
+
+    D_output = D(x_real)
+    D_real_loss = criterion(D_output, y_real)
+    
+
+    # train discriminator on facke
+    z = Variable(torch.randn(bs, z_dim).to(device))
+    x_fake, y_fake = G(z), Variable(torch.zeros(bs, 1).to(device))
+
+    D_output = D(x_fake)
+    D_fake_loss = criterion(D_output, y_fake)
+    
+
+    # gradient backprop & optimize ONLY D's parameters
+    D_loss = D_real_loss + D_fake_loss
+    D_loss.backward()
+    D_optimizer.step()
+        
+    return  D_loss.data.item()
+
+
+def G_train():
+    #=======================Train the generator=======================#
+    G.zero_grad()
+
+    z = Variable(torch.randn(bs, z_dim).to(device))
+    y = Variable(torch.ones(bs, 1).to(device))
+
+    G_output = G(z)
+    D_output = D(G_output)
+    G_loss = criterion(D_output, y)
+
+    # gradient backprop & optimize ONLY G's parameters
+    G_loss.backward()
+    G_optimizer.step()
+        
+    return G_loss.data.item()
+
+
+# Plot the loss from each batch
+def plotLoss(epoch):
+    plt.figure(figsize=(10, 8))
+    plt.plot(dLosses, label='Discriminitive loss')
+    plt.plot(gLosses, label='Generative loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.savefig('gan_loss_epoch_%d.png' % epoch)
+
+# Create a wall of generated MNIST images
+def plotGeneratedImages(generatedImages,dim=(10, 10), figsize=(10, 10)):
+    generatedImages=generatedImages.cpu().numpy()
+    print(generatedImages.shape)
+    plt.figure(figsize=figsize)
+    for i in range(generatedImages.shape[0]):
+        plt.subplot(dim[0], dim[1], i+1)
+        plt.imshow(np.squeeze(generatedImages[i]), interpolation='nearest', cmap='gray')
+        plt.axis('off')
+    plt.tight_layout()
+    plt.savefig('gan_generated_image_epoch_%d.png' % epoch)
+    IPython.display.display(IPython.display.Image(data=('gan_generated_image_epoch_%d.png' % epoch)))
+
+
+
+n_epoch = 20
+for epoch in range(1, n_epoch+1):           
+    D_losses, G_losses = [], []
+    for batch_idx, (x, _) in enumerate(train_loader):
+        D_losses.append(D_train(x))
+        G_losses.append(G_train(x))
+
+    print('[%d/%d]: loss_d: %.3f, loss_g: %.3f' % (
+            (epoch), n_epoch, torch.mean(torch.FloatTensor(D_losses)), torch.mean(torch.FloatTensor(G_losses))))
+    with torch.no_grad():
+        test_z = Variable(torch.randn(bs, z_dim).to(device))
+        generated = G(test_z)
+        plotGeneratedImages(generated.view(generated.size(0), 1, 28, 28))
